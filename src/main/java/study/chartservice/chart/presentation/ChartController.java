c90ffabd2fcf2b7e200c339424abf6fdaf64eeea
ddohyeong <dohyeong7026@gmail.com>
2024-05-26 16:14:14 +0900

    Feat: 일, 주, 월, 년 차트 데이터 조회 기능 구현

--- src/main/java/study/chartservice/chart/presentation/ChartController.java
+++ src/main/java/study/chartservice/chart/presentation/ChartController.java
@@ -1,11 +1,14 @@
 package study.chartservice.chart.presentation;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.chartservice.chart.application.ChartService;
 import study.chartservice.chart.application.CompanyInfoService;
+import study.chartservice.chart.dto.resp.StockDto;
 import study.chartservice.chart.vo.resp.StockNameVo;
 import study.chartservice.global.common.response.BaseResponse;
 
@@ -15,9 +18,10 @@ public class ChartController {
 
 	private final CompanyInfoService companyInfoService;
 	private final ModelMapper modelMapper;
+	private final ChartService chartService;
 
 	// 주식 이름 조회
-	@GetMapping("/stockitem/{stockCode}/name")
+	@GetMapping("/{stockCode}/name")
 	public BaseResponse<StockNameVo> getCompanyName(
 			@PathVariable("stockCode") String stockCode
 	) {
@@ -26,4 +30,31 @@ public class ChartController {
 						StockNameVo.class));
 	}
 
+	@GetMapping("/chart/{stockCode}/day")
+	public BaseResponse<List<StockDto>> getChartOfDayByStockCode(
+			@PathVariable("stockCode") String stockCode
+	) {
+		return new BaseResponse<>(chartService.getChartOfDayByStockCode(stockCode));
+	}
+
+	@GetMapping("/chart/{stockCode}/week")
+	public BaseResponse<List<StockDto>> getChartOfWeekByStockCode(
+			@PathVariable("stockCode") String stockCode
+	) {
+		return new BaseResponse<>(chartService.getChartOfWeekByStockCode(stockCode));
+	}
+
+	@GetMapping("/chart/{stockCode}/month")
+	public BaseResponse<List<StockDto>> getChartOfMonthByStockCode(
+			@PathVariable("stockCode") String stockCode
+	) {
+		return new BaseResponse<>(chartService.getChartOfMonthByStockCode(stockCode));
+	}
+
+	@GetMapping("/chart/{stockCode}/year")
+	public BaseResponse<List<StockDto>> getChartOfYearByStockCode(
+			@PathVariable("stockCode") String stockCode
+	) {
+		return new BaseResponse<>(chartService.getChartOfYearByStockCode(stockCode));
+	}
 }
