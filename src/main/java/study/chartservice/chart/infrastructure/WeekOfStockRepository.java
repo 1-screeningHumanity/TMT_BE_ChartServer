df66138b74ef192dfd68d59e07e519d307df707f
ddohyeong <dohyeong7026@gmail.com>
2024-06-12 15:18:53 +0900

    Feat: 주식 데이터 조회 시 날짜별 내림차순 정렬

--- src/main/java/study/chartservice/chart/infrastructure/WeekOfStockRepository.java
+++ src/main/java/study/chartservice/chart/infrastructure/WeekOfStockRepository.java
@@ -2,9 +2,11 @@ package study.chartservice.chart.infrastructure;
 
 import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import study.chartservice.chart.domain.WeekOfStock;
 
 public interface WeekOfStockRepository extends MongoRepository<WeekOfStock, String> {
+	@Query(sort = "{ 'stck_bsop_date' : -1 }")
 	List<WeekOfStock> findByStockCode(String stockCode);
 
 }
