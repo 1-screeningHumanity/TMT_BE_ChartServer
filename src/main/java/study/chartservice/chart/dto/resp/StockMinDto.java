42e2ff9a76756825bd7a42a7c17c9835c975371c
ddohyeong <dohyeong7026@gmail.com>
2024-06-14 14:22:07 +0900

    Feat: 30분 주식 분봉 데이터 응답값 수정

--- src/main/java/study/chartservice/chart/dto/resp/StockMinDto.java
+++ src/main/java/study/chartservice/chart/dto/resp/StockMinDto.java
@@ -5,9 +5,13 @@ import lombok.Getter;
 @Getter
 public class StockMinDto {
 	private String stockCode;   // 주식 코드
-	private String stck_cntg_hour; //	주식 체결 시간
-	private String stck_prpr; //	주식 현재가
-	private String stck_hgpr; //	주식 최고가
-	private String stck_lwpr; //	주식 최저가
-	private String cntg_vol; //	체결 거래량
+	private String stockCreatAt; // 주식 영업 일자
+	private String prdy_vrss;   // 	전일 대비
+	private String prdy_vrss_sign;  //  전일 대비 부호
+	private String prdy_ctrt;   // 전일 대비율
+	private String stck_prdy_clpr;  //  주식 전일 종가
+	private String acml_vol;    // 누적 거래량
+	private String acml_tr_pbmn;    //  누적 거래 대금
+	private String hts_kor_isnm;    // HTS 한글 종목명
+	private String stck_prpr;   // 주식 현재가
 }
