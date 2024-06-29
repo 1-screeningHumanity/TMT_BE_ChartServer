e3105eef1aa9c7e808ab074ffdaea47cd310bc14
ddohyeong <dohyeong7026@gmail.com>
2024-06-29 14:40:46 +0900

    feat: 주식 데이터 스케줄링 에러 시 보상 로직 구현

--- src/main/java/study/chartservice/ChartServiceApplication.java
+++ src/main/java/study/chartservice/ChartServiceApplication.java
@@ -2,14 +2,14 @@ package study.chartservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableRetry
 public class ChartServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ChartServiceApplication.class, args);
 	}
-
 }
